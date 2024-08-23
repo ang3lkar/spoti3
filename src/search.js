66117baff47eb3d3359830359364bccc05333f76
Angelos Karagkiozidis <angelkar@gmail.com>
2024-08-23 10:02:24 +0300

    refactor: update search

--- src/search.js
+++ src/search.js
@@ -6,17 +6,21 @@ const url = "https://www.googleapis.com/youtube/v3/search";
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
+const searchParams = {
+  part: "snippet",
+  type: "video",
+  videoCategoryId: "10", // Category ID for Music
+  key: API_KEY,
+  maxResults: 1,
+}
+
 // Function to search for a video on YouTube
 export async function searchYouTube(query) {
   try {
     const response = await axios.get(url, {
       params: {
-        part: "snippet",
-        q: query,
-        type: "video",
-        videoCategoryId: "10", // Category ID for Music
-        key: API_KEY,
-        maxResults: 1,
+        ...searchParams,
+        q: query
       },
     });
 
@@ -24,6 +28,7 @@ export async function searchYouTube(query) {
 
     if (items.length > 0) {
       const video = items[0];
+
       return {
         videoId: video.id.videoId,
         title: video.snippet.title,
