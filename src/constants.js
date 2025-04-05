7a3a5413fc06101c716b7693b522690bb195e7b1
Angelos Karagkiozidis <angelos.kar@theblueground.com>
2025-04-05 21:07:34 +0300

    fix: avoid colors on marks

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ import chalk from "chalk";
 export const DOWNLOADS_FOLDER = "downloads";
 export const PLAYLISTS_FOLDER = "playlists";
 
-export const checkMark = chalk.green("✔");
-export const failMark = chalk.red("✗");
+export const checkMark = "✔";
+export const failMark = "✗";
